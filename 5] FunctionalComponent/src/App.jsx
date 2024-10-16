ffdd7a721748fe7f917070097c06f498ebdb7a36
Sachin-Ramani <167863095+Sachin-Ramani@users.noreply.github.com>
2024-10-16 15:25:30 +0530

    Add files via upload

---
+++ 5] FunctionalComponent/src/App.jsx
@@ -0,0 +1,12 @@
+import FunctionalComponent from './Components/FunctionalComponent'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <FunctionalComponent />
+    </>
+  )
+}
+
+export default App
